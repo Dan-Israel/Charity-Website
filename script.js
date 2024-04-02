0c2083ba4e626a777b03b5dc86c485a93632f1bf
Dan-Israel <113846233+Dan-Israel@users.noreply.github.com>
2024-04-02 02:04:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let open = document.getElementById("myNav");
+
+
+
+function openNav() {
+     open.style.display = "block";
+   }
+  
+  function closeNav() {
+    open.style.display = "none";
+  }
+  
